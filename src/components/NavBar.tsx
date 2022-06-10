44a5a7b115f2921ff8108f6f2da065159cf8fc06
Eslashier <anfelov@gmail.com>
2022-06-10 08:46:51 -0500

    updated logo an styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,18 @@
-import * as React from 'react'
 import { Link } from 'react-router-dom'
-import {useSelector} from "react-redux";
-import "./styles.css";
+import logo from '../imgs/Logo.png'
+import "./stylesNavBar.css";
 
 export default function Navbar() {
   return (
     <nav className="nav">
       <Link to="/" className="site-title">
-        Raul Hardware Store
+        <img style={{height: 50}} src={logo}/>
       </Link>
       <ul>
         <Link to="/">Home</Link>
         <Link to="/ProductList">Product List</Link>
         <Link to="/ProductListAll">Product List 2</Link>
-        <button className="">Log out</button>
+        <button className="logout">Log out</button>
       </ul>
     </nav>
   )
