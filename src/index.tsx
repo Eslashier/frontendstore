1aefacb32e65541ece5f2673aea1004f03a13ca5
Eslashier <anfelov@gmail.com>
2022-06-09 21:18:31 -0500

    Firtst header created with routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import StoreProvider from './state/StoreProvider'
+import {Provider} from "react-redux";
+import {store} from "./state/store";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <StoreProvider>
-      <App />
-    </StoreProvider>
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>
 )
+ 
